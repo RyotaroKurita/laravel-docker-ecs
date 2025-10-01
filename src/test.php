19e33daa68b5965f78bac57f58b651d08f9887c3
RyotaroKurita <kurita@reclick.co.jp>
2025-10-01 13:38:09 +0900

    commit時: Huskyをmy_test.phpファイルに対して実行

---
+++ src/test.php
@@ -0,0 +1,18 @@
+<!-- 世界一汚いPHPのソースコード -->
+<?php
+echo "Hello World!";
+if     (true)
+
+
+{
+       echo "Hello World!";
+}
+
+
+
+echo 'Hello World!';
+
+$num = 1;
+if
+($num>=10)      { echo "The variable num is a number greater than or equal to 10.";}else {        echo 'The variable num is a number less than 10.';}
+?>
